6ac032f0f2ac6c814d0a834a795ba3d8cecdc15d
K-Wei <kevinwei@users.noreply.github.com>
2017-01-25 16:54:27 -0600

    functional
    all options work, not polished

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,67 @@
+class GamesController < ApplicationController
+
+  def play_rock
+
+    @outcome = nil
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "tie"
+    elsif @computer_move == "paper"
+      @outcome = "lose"
+    elsif @computer_move == "scissors"
+      @outcome = "win"
+    end
+
+    render("games/play_rock.html.erb")
+  end
+
+  def play_paper
+
+    @outcome = nil
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "win"
+    elsif @computer_move == "paper"
+      @outcome = "tie"
+    elsif @computer_move == "scissors"
+      @outcome = "lose"
+    end
+
+    render("games/play_paper.html.erb")
+  end
+
+  def play_scissors
+
+    @outcome = nil
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "tie"
+    elsif @computer_move == "paper"
+      @outcome = "lose"
+    elsif @computer_move == "scissors"
+      @outcome = "win"
+    end
+
+    render("games/play_rock.html.erb")
+  end
+
+  def play_paper
+
+    @outcome = nil
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "lose"
+    elsif @computer_move == "paper"
+      @outcome = "win"
+    elsif @computer_move == "scissors"
+      @outcome = "tie"
+    end
+
+    render("games/play_scissors.html.erb")
+  end
+
+end

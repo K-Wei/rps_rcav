bb56c752e78bbd5b78a1479e27c302f6f674d9eb
K-Wei <kevinwei@users.noreply.github.com>
2017-01-30 17:39:39 -0600

    added root url, attempted to polish
    i struggle with front-end design. added root url, images, buttons,
    formatting, and general polish

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,10 +6,13 @@ class GamesController < ApplicationController
     @computer_move = ["rock", "paper", "scissors"].sample
 
     if @computer_move == "rock"
+      @computer_icon = "fa-hand-rock-o"
       @outcome = "tie"
     elsif @computer_move == "paper"
+      @computer_icon = "fa-hand-paper-o"
       @outcome = "lose"
     elsif @computer_move == "scissors"
+      @computer_icon = "fa-hand-scissors-o"
       @outcome = "win"
     end
 
@@ -22,10 +25,13 @@ class GamesController < ApplicationController
     @computer_move = ["rock", "paper", "scissors"].sample
 
     if @computer_move == "rock"
+      @computer_icon = "fa-hand-rock-o"
       @outcome = "win"
     elsif @computer_move == "paper"
+      @computer_icon = "fa-hand-paper-o"
       @outcome = "tie"
     elsif @computer_move == "scissors"
+      @computer_icon = "fa-hand-scissors-o"
       @outcome = "lose"
     end
 
@@ -38,30 +44,23 @@ class GamesController < ApplicationController
     @computer_move = ["rock", "paper", "scissors"].sample
 
     if @computer_move == "rock"
-      @outcome = "tie"
-    elsif @computer_move == "paper"
-      @outcome = "lose"
-    elsif @computer_move == "scissors"
-      @outcome = "win"
-    end
-
-    render("games/play_rock.html.erb")
-  end
-
-  def play_paper
-
-    @outcome = nil
-    @computer_move = ["rock", "paper", "scissors"].sample
-
-    if @computer_move == "rock"
+      @computer_icon = "fa-hand-rock-o"
       @outcome = "lose"
     elsif @computer_move == "paper"
+      @computer_icon = "fa-hand-paper-o"
       @outcome = "win"
     elsif @computer_move == "scissors"
+      @computer_icon = "fa-hand-scissors-o"
       @outcome = "tie"
     end
 
     render("games/play_scissors.html.erb")
+  end
+
+  def display_intro
+
+    render("games/display_intro.html.erb")
+
   end
 
 end
